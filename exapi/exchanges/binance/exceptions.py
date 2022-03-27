2f3b04af3e8e837d08f777825ab227f7aea4ec4f
astsu <you@example.com>
2022-03-27 17:38:00 +0300

    feat: Add `BinanceAuthError`, `BinanceBadPrecisionError`, `BinanceBadRecvWindowError` binance exceptions.

--- exapi/exchanges/binance/exceptions.py
+++ exapi/exchanges/binance/exceptions.py
@@ -1,5 +1,4 @@
 from exapi.exceptions import ExchangeError
-from exapi.models import Request, Response
 
 
 class BinanceError(ExchangeError):
@@ -12,10 +11,26 @@ class BinanceError(ExchangeError):
 class BinanceInvalidSymbolError(BinanceError):
     """Will be raised when symbol does not exist on exchange."""
 
-    def __init__(
-        self, request: Request, response: Response
-    ) -> None:
-        super().__init__(request, response)
-
     def __str__(self) -> str:
         return "Invalid symbol."
+
+
+class BinanceAuthError(BinanceError):
+    """Will be raised when account api keys are not valid."""
+
+    def __str__(self) -> str:
+        return "Invalid api keys."
+
+
+class BinanceBadPrecisionError(BinanceError):
+    """Will be raised when precision is over the maximum defined for this asset."""
+
+    def __str__(self) -> str:
+        return "Precision is over the maximum defined for this asset."
+
+
+class BinanceBadRecvWindowError(BinanceError):
+    """Will be raised when recv window is not valid."""
+
+    def __str__(self) -> str:
+        return "Recv window must be less than 60000."
