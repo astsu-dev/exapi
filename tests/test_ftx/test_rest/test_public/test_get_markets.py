f3ec01ca7c84e788137760b2b522d1b4973b1132
astsu <you@example.com>
2022-03-28 13:02:20 +0300

    test: Add tests for `FTXREST.get_markets` method.

--- tests/test_ftx/test_rest/test_public/test_get_markets.py
+++ tests/test_ftx/test_rest/test_public/test_get_markets.py
@@ -1,10 +1,17 @@
 import pytest
 
-from exapi.exchanges.ftx.rest import FTXRESTWithoutCredentials
+from exapi.exchanges.ftx.rest import FTXREST, FTXRESTWithoutCredentials
 
 
 @pytest.mark.asyncio
 async def test_ftx_rest_without_credentials_get_markets() -> None:
     async with FTXRESTWithoutCredentials() as rest:
+        result = await rest.get_markets()
+    assert {"name", "baseCurrency", "quoteCurrency"}.issubset(set(result[0]))
+
+
+@pytest.mark.asyncio
+async def test_ftx_rest_get_markets() -> None:
+    async with FTXREST() as rest:
         result = await rest.get_markets()
     assert {"name", "baseCurrency", "quoteCurrency"}.issubset(set(result[0]))
