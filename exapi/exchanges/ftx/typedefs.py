9218269d26f2e766dc8deb3041bbbb3dec651a7c
astsu <you@example.com>
2022-03-28 11:46:01 +0300

    feat: Add FTX exchange typedefs.

---
+++ exapi/exchanges/ftx/typedefs.py
@@ -0,0 +1,28 @@
+from typing import Literal, TypedDict
+
+FTXMarketType = Literal["spot", "future"]
+
+
+class FTXMarket(TypedDict):
+    name: str
+    baseCurrency: str | None
+    quoteCurrency: str | None
+    quoteVolume24h: float
+    change1h: float
+    change24h: float
+    changeBod: float
+    highLeverageFeeExempt: bool
+    minProvideSize: float
+    type: FTXMarketType
+    underlying: str
+    enabled: bool
+    ask: float
+    bid: float
+    last: float
+    postOnly: bool
+    price: float
+    priceIncrement: float
+    sizeIncrement: float
+    restricted: bool
+    volumeUsd24h: float
+    largeOrderThreshold: float
