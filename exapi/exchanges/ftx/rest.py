65d551ea64c1161ad01a15e1ffa59a7298923734
astsu <you@example.com>
2022-03-28 13:01:56 +0300

    feat: Add `FTXREST` class with `get_markets` method.

--- exapi/exchanges/ftx/rest.py
+++ exapi/exchanges/ftx/rest.py
@@ -1,9 +1,11 @@
-from typing import Any, cast
+from types import TracebackType
+from typing import Any, Type, cast
 
 import aiohttp
 
 from exapi.base.rest import BaseExchangeREST
 from exapi.exchanges.ftx.exceptions import FTXError, FTXInvalidMarketError
+from exapi.exchanges.ftx.models import FTXCredentials
 from exapi.exchanges.ftx.typedefs import FTXMarket
 from exapi.models import Request, Response
 from exapi.typedefs import HeadersType
@@ -89,3 +91,87 @@ class FTXRESTWithoutCredentials(BaseExchangeREST):
         if error.startswith("no such market"):
             raise FTXInvalidMarketError(request=request, response=response_info)
         raise FTXError(request=request, response=response_info)
+
+
+class FTXREST:
+    """This class is a wrapper around `FTXRESTWithoutCredentials`.
+    It incapsulates account credentials in `__init__` method.
+    It can be useful if you need to manage only one account.
+    For private methods you don't need to provide credentials in params.
+    If you need to manage multiple accounts use `FTXRESTWithoutCredentials`.
+    """
+
+    def __init__(
+        self,
+        api_key: str = "",
+        api_secret: str = "",
+        subaccount: str | None = None,
+        *,
+        base_url: str = "https://ftx.com",
+        request_timeout: float | None = None,
+    ) -> None:
+        self._credentials = FTXCredentials(api_key, api_secret, subaccount)
+        self._client = FTXRESTWithoutCredentials(
+            base_url=base_url, request_timeout=request_timeout
+        )
+
+    async def get_markets(self) -> list[FTXMarket]:
+        """Returns list of markets.
+
+        >>> import asyncio
+        >>> from exapi.exchanges.ftx.rest import FTXREST
+        >>>
+        >>> async def get_markets() -> list[FTXMarket]:
+        ...     async with FTXREST() as rest:
+        ...         return await rest.get_markets()
+        ...
+        >>> asyncio.run(get_markets())
+        [
+            {
+                "name": "BTC-PERP",
+                "baseCurrency": None,
+                "quoteCurrency": None,
+                "quoteVolume24h": 28914.76,
+                "change1h": 0.012,
+                "change24h": 0.0299,
+                "changeBod": 0.0156,
+                "highLeverageFeeExempt": False,
+                "minProvideSize": 0.001,
+                "type": "future",
+                "underlying": "BTC",
+                "enabled": True,
+                "ask": 3949.25,
+                "bid": 3949,
+                "last": 10579.52,
+                "postOnly": False,
+                "price": 10579.52,
+                "priceIncrement": 0.25,
+                "sizeIncrement": 0.0001,
+                "restricted": False,
+                "volumeUsd24h": 28914.76,
+                "largeOrderThreshold": 5000
+            }
+        ]
+
+        Returns:
+            List of markets.
+        """
+
+        return await self._client.get_markets()
+
+    async def close(self) -> None:
+        """Closes session."""
+
+        await self._client.close()
+
+    async def __aenter__(self) -> "FTXREST":
+        self._client = await self._client.__aenter__()
+        return self
+
+    async def __aexit__(
+        self,
+        exc_type: Type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> None:
+        await self._client.__aexit__(exc_type, exc_val, exc_tb)
