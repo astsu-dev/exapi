1b800c4cc431d6cfffccbe3c9947b781daf38435
astsu <you@example.com>
2022-03-29 12:05:36 +0300

    feat: Add typedefs for Bybit exchange.

---
+++ exapi/exchanges/bybit/typedefs.py
@@ -0,0 +1,54 @@
+from typing import Literal, TypedDict
+
+BybitOrderStatus = Literal[
+    "NEW",
+    "PARTIALLY_FILLED",
+    "FILLED",
+    "CANCELED",
+    "PENDING_CANCEL",
+    "PENDING_NEW",
+    "REJECTED",
+]
+BybitOrderType = Literal["LIMIT", "MARKET", "LIMIT_MAKER"]
+BybitOrderSide = Literal["Buy", "Sell"]
+BybitTimeInForce = Literal["GTC", "FOK", "IOC"]
+
+
+class BybitSymbolInfo(TypedDict):
+    name: str
+    alias: str
+    baseCurrency: str
+    quoteCurrency: str
+    basePrecision: str
+    quotePrecision: str
+    minTradeQuantity: str
+    minTradeAmount: str
+    minPricePrecision: str
+    maxTradeQuantity: str
+    maxTradeAmount: str
+    category: int
+
+
+class BybitSymbolTicker(TypedDict):
+    symbol: str
+    bidPrice: str
+    bidQty: str
+    askPrice: str
+    askQty: str
+    time: int
+
+
+class BybitOrder(TypedDict):
+    accountId: str
+    symbol: str
+    symbolName: str
+    orderLinkId: str
+    orderId: str
+    transactTime: str
+    price: str
+    origQty: str
+    executedQty: str
+    status: BybitOrderStatus
+    timeInForce: BybitTimeInForce
+    type: BybitOrderType
+    side: BybitOrderSide
