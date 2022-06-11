05021db122767c4cfc8571a1ff3314600575b373
astsu <you@example.com>
2022-06-11 09:33:24 +0300

    refactor: Remove unused code.

--- tests/test_bybit/test_rest/test_private/test_new_order.py
+++ tests/test_bybit/test_rest/test_private/test_new_order.py
@@ -1,12 +1,8 @@
-import os
-import pprint
-
 import pytest
 from dotenv import load_dotenv
 
 from exapi.exchanges.bybit.exceptions import (
     BybitAuthError,
-    BybitError,
     BybitInsufficientBalanceError,
     BybitInvalidPriceDecimalsError,
     BybitInvalidQuantityDecimalsError,
@@ -14,42 +10,8 @@ from exapi.exchanges.bybit.exceptions import (
 )
 from exapi.exchanges.bybit.models import BybitCredentials
 from exapi.exchanges.bybit.rest import BybitREST
-from exapi.exchanges.bybit.typedefs import BybitOrder
 
 load_dotenv()
-API_KEY = os.getenv("BYBIT_TEST_API_KEY", "")
-API_SECRET = os.getenv("BYBIT_TEST_API_SECRET", "")
-
-
-def error_handler(fn):
-    async def wrapper(*args, **kwargs):
-        try:
-            return await fn(*args, **kwargs)
-        except BybitError as e:
-            pprint.pprint(e.response)
-            raise
-
-    return wrapper
-
-
-@error_handler
-async def bybit_new_limit_order() -> BybitOrder:
-    async with BybitREST(API_KEY, API_SECRET) as rest:
-        return await rest.new_order(
-            symbol="BITUSDT",
-            side="Buy",
-            order_type="LIMIT",
-            quantity="10",
-            price="0.1",
-        )
-
-
-@error_handler
-async def bybit_new_market_order() -> BybitOrder:
-    async with BybitREST(API_KEY, API_SECRET) as rest:
-        return await rest.new_order(
-            symbol="BITUSDT", side="Buy", order_type="MARKET", quantity="25.76"
-        )
 
 
 @pytest.mark.asyncio
